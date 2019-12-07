1dce4b9447441c028eca5ed7b9742dd8bcd5ca76
Evan Kaiser <733581+holotrek@users.noreply.github.com>
2019-12-07 18:21:42 -0500

    Simplifies setup to use environment vars instead of settings

--- src/server.js
+++ src/server.js
@@ -1,40 +1,16 @@
-import fs from 'fs';
-import chalk from 'chalk';
 import express from 'express';
 import cors from 'cors';
 import http from 'http';
-import https from 'https';
 import bodyParser from 'body-parser';
-import merge from 'deepmerge';
-import moment from 'moment';
 import { HelloService } from './hello-service.js';
 
 const app = express();
 const router = express.Router();
-const env = process.env.NODE_ENV || 'Development';
-const generalSettings = require('./appsettings.json');
-const envSettings = require(`./appsettings.${env}.json`);
-const settings = merge(generalSettings, envSettings);
-
-let httpServer;
-let httpsServer;
-if ((settings.server.http || {}).enabled) {
-    httpServer = http.createServer(app);
-}
-if ((settings.server.https || {}).enabled) {
-    httpsServer = https.createServer(
-        {
-            key: fs.readFileSync(settings.server.https.key),
-            cert: fs.readFileSync(settings.server.https.cert)
-        },
-        app
-    );
-}
-
+const httpServer = http.createServer(app);
 app.use(bodyParser.urlencoded({ extended: true }))
     .use(
         cors({
-            origin: settings.allowedOrigin
+            origin: process.env.ORIGINS || '*'
         })
     )
     .use(bodyParser.json())
@@ -45,14 +21,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
         <h2>Version: ${pkg.version}</h2>`);
     });
 
-if (httpServer) {
-    httpServer.listen(settings.server.http.port);
-    console.log(`Listening on http://localhost:${settings.server.http.port}`);
-}
-if (httpsServer) {
-    httpsServer.listen(settings.server.https.port);
-    console.log(`Listening on https://localhost:${settings.server.https.port}`);
-}
+const port = process.env.NODE_PORT || 3000;
+httpServer.listen(port);
+console.log(`Listening on http://localhost:${port}`);
 
 // Setup routes
 router.get('/test', async (req, res) => {
