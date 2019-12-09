8723150b3eb15d075f399ef0f9a0ec17c9ac4f7f
Evan Kaiser <733581+holotrek@users.noreply.github.com>
2019-12-09 09:38:30 -0500

    Reverted back to not use scripts since it wasn't working

--- src/scripts/build_image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export FULL_VERSION=$VERSION_PREFIX.$TRAVIS_BUILD_NUMBER
-export REPO_TAGGED=$TRAVIS_REPO_SLUG:$FULL_VERSION
-export REPO_LATEST=$TRAVIS_REPO_SLUG:latest
-echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t $REPO_TAGGED .
--- src/scripts/push_image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker tag $REPO_TAGGED $REPO_LATEST
-docker push $REPO_TAGGED
-docker push $REPO_LATEST
