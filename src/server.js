a6e797953c749675e0908cf87bd748925aa2793f
Evan Kaiser <733581+holotrek@users.noreply.github.com>
2020-03-16 08:26:47 -0400

    Adds db/q connections and status page to check them without deploying actual apps.

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import http from 'http';
 import bodyParser from 'body-parser';
+import amqp from 'amqplib';
 import * as awilix from 'awilix';
+import { MongoClient } from 'mongodb';
 import { HelloService } from './services/hello-service.js';
 import { HelloController } from './controllers/hello-controller.js';
 
@@ -11,6 +13,46 @@ const container = awilix.createContainer({
 });
 
 container.register({
+    mongodbProvider: awilix.asFunction(() => {
+        if (!process.env.MONGODB_CONNECTION) {
+            throw new Error('An environment variable MONGODB_CONNECTION is required with the value of the db connection string.');
+        }
+        return {
+            connect: async () => {
+                return new Promise((res, rej) => {
+                    try {
+                        MongoClient.connect(
+                            process.env.MONGODB_CONNECTION,
+                            {
+                                useUnifiedTopology: true
+                            },
+                            (err, client) => {
+                                if (err) rej(err);
+                                else {
+                                    res(client.db(process.env.MONGODB_NAME || 'database'));
+                                }
+                            }
+                        );
+                    } catch (err) {
+                        rej(err);
+                    }
+                });
+            }
+        };
+    }),
+    pubSubQueueProvider: awilix.asFunction(() => {
+        if (!process.env.RABBITMQ_PUBSUB_CONNECTION) {
+            throw new Error(
+                'An environment variable RABBITMQ_PUBSUB_CONNECTION is required with the value of the queue connection string.'
+            );
+        }
+        return {
+            connect: async () => {
+                const conn = await amqp.connect(process.env.RABBITMQ_PUBSUB_CONNECTION);
+                return await conn.createChannel();
+            }
+        };
+    }),
     helloController: awilix.asClass(HelloController),
     helloService: awilix.asClass(HelloService)
 });
@@ -30,6 +72,28 @@ app.use(bodyParser.urlencoded({ extended: true }))
         const pkg = require('../package.json');
         res.send(`<h1>${pkg.name}</h1>
         <h2>Version: ${pkg.version}</h2>`);
+    })
+    .get('/status', async (req, res) => {
+        let result = '';
+        try {
+            await container.cradle.mongodbProvider.connect();
+            result += 'DB connection: Successful<br />';
+        } catch (err) {
+            result += `DB connection: Error: ${err}<br />`;
+        }
+
+        try {
+            var channel = await container.cradle.pubSubQueueProvider.connect();
+            if (channel) {
+                result += 'Queue channel connection: Successful<br />';
+            } else {
+                result += 'Queue channel connection: Failed<br />';
+            }
+            channel.close();
+        } catch (err) {
+            result += `Queue channel connection: ${err}<br />`;
+        }
+        res.send(result);
     });
 
 const port = process.env.NODE_PORT || 3000;
